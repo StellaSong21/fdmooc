c2067616be2e5b8f234f86cbac5288ad51cd0e7c
HypnosRin <1092534779@qq.com>
2018-07-20 23:12:26 +0800

    huaq

---
+++ src/main/java/servlet/Servlet.java
@@ -0,0 +1,21 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Servlet extends HttpServlet {
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        ;
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
